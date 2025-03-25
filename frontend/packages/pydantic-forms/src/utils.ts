a1a664b54458c74bd6a39335bbaa188915e236d4
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2025-03-25 12:41:10 +0100

    1682 Moves getHashForArray function to the utils file

--- frontend/packages/pydantic-forms/src/utils.ts
+++ frontend/packages/pydantic-forms/src/utils.ts
@@ -16,3 +16,15 @@ export const preventDefault = (fn: () => void) => (e: React.MouseEvent) => {
     e.preventDefault();
     fn();
 };
+
+export const getHashForArray = async (array: object[]) => {
+    const arrayString = JSON.stringify(array);
+    const arrayBuffer = new TextEncoder().encode(arrayString);
+
+    const digest = await crypto.subtle.digest('SHA-256', arrayBuffer);
+    const hashHex = Array.from(new Uint8Array(digest), (b) =>
+        b.toString(16).padStart(2, '0'),
+    ).join('');
+
+    return hashHex;
+};
