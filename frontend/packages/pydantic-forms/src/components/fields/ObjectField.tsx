d24ccad5981c36f11080bf0bc94d119ac1c5c6da
Georgi2704 <georgi.manev@surf.nl>
2025-05-26 16:05:31 +0200

    Wrapper to customize object field

--- frontend/packages/pydantic-forms/src/components/fields/ObjectField.tsx
+++ frontend/packages/pydantic-forms/src/components/fields/ObjectField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC, ReactNode } from 'react';
 
 import { usePydanticFormContext } from '@/core';
 import { componentsMatcher } from '@/core';
@@ -6,9 +6,14 @@ import { PydanticFormElementProps } from '@/types';
 
 import { RenderFields } from '../render';
 
+export type ObjectFieldProps = PydanticFormElementProps & {
+    customObjectFieldWrapper?: FC<{ children: ReactNode }>;
+};
+
 export const ObjectField = ({
-    pydanticFormField,
-}: PydanticFormElementProps) => {
+                                pydanticFormField,
+                                customObjectFieldWrapper: CustomObjectFieldWrapper,
+                            }: ObjectFieldProps) => {
     const { config } = usePydanticFormContext();
 
     const components = componentsMatcher(
@@ -16,19 +21,25 @@ export const ObjectField = ({
         config?.componentMatcher,
     );
 
-    return (
-        <div
-            style={{
-                border: 'thin dotted grey',
-                padding: '1rem',
-                marginTop: '16px',
-                display: 'flex',
-                flexDirection: 'column',
-                flexGrow: 1,
-            }}
-        >
-            <h1>{pydanticFormField.title}</h1>
-            <RenderFields pydanticFormComponents={components} />
-        </div>
-    );
+    const content = <RenderFields pydanticFormComponents={components} />;
+
+    return CustomObjectFieldWrapper ? (
+        <CustomObjectFieldWrapper>{content}</CustomObjectFieldWrapper>
+    ) : (
+        <>
+            <div
+                style={{
+                    border: 'thin dotted grey',
+                    padding: '1rem',
+                    marginTop: '16px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flexGrow: 1,
+                }}
+            >
+                <h1>{pydanticFormField.title}</h1>
+                {content}
+            </div>
+        </>
+    )
 };
