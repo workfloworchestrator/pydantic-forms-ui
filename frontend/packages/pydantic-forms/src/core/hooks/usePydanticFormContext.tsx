58411b1c206995f38b5b4d9cd7d2c43ccde0cbee
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-06-24 14:47:24 +0200

    Improves null check in context hook

--- frontend/packages/pydantic-forms/src/core/hooks/usePydanticFormContext.tsx
+++ frontend/packages/pydantic-forms/src/core/hooks/usePydanticFormContext.tsx
@@ -5,7 +5,7 @@ import { PydanticFormContext } from '@/core/PydanticFormContextProvider';
 export function usePydanticFormContext() {
     const context = useContext(PydanticFormContext);
 
-    if (!context) {
+    if (context === null || context === undefined) {
         throw new Error(
             'usePydanticFormContext must be used within a PydanticFormProvider',
         );
