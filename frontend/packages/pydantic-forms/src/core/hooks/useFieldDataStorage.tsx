270efe337533b4723302bb2e52be0fbc43d17ce9
Ruben van Leeuwen <ruben@sinin.nl>
2025-09-17 15:22:23 +0200

    Update frontend/packages/pydantic-forms/src/core/hooks/useFieldDataStorage.tsx
    Co-authored-by: tjeerddie <tjeerdverschragen@hotmail.com>

--- frontend/packages/pydantic-forms/src/core/hooks/useFieldDataStorage.tsx
+++ frontend/packages/pydantic-forms/src/core/hooks/useFieldDataStorage.tsx
@@ -3,6 +3,5 @@ import { useContext } from 'react';
 import { PydanticFormFieldDataStorageContext } from '../PydanticFieldDataStorageProvider';
 
 export const useGetFieldDataStorage = () => {
-    const fieldDataStorage = useContext(PydanticFormFieldDataStorageContext);
-    return fieldDataStorage;
+    return useContext(PydanticFormFieldDataStorageContext);
 };
