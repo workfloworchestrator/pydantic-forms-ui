9a4ebd2704d68b0cb97a3ac6412599495a523aab
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-09-17 15:25:16 +0200

    2143: PR comments

--- frontend/packages/pydantic-forms/src/core/hooks/useGetValidationErrors.tsx
+++ frontend/packages/pydantic-forms/src/core/hooks/useGetValidationErrors.tsx
@@ -3,6 +3,5 @@ import { useContext } from 'react';
 import { PydanticFormValidationErrorContext } from '@/PydanticForm';
 
 export const useGetValidationErrors = () => {
-    const context = useContext(PydanticFormValidationErrorContext);
-    return context;
+    return useContext(PydanticFormValidationErrorContext);
 };
