883434b3acc32d1119b0e3cabd9e9cea7f1eb733
Ruben van Leeuwen <ruben@sinin.nl>
2026-01-14 14:15:50 +0100

    237 nullable int (#238)
    * 237: Allow sending null when field isNullable
    
    * 237: Adds changeset
    
    * 237: More better readable
    
    ---------
    
    Co-authored-by: Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>

--- frontend/packages/pydantic-forms/src/components/fields/IntegerField.tsx
+++ frontend/packages/pydantic-forms/src/components/fields/IntegerField.tsx
@@ -12,20 +12,30 @@ export const IntegerField = ({
     disabled,
     pydanticFormField,
 }: PydanticFormControlledElementProps) => {
-    // If the field is part of an array the value is passed in as an object with the field name as key
-    // this is imposed by react-hook-form. We try to detect this and extract the actual value
     const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
-    const fieldValue =
-        _.isObject(value) && _.has(value, fieldName)
-            ? _.get(value, fieldName)
-            : value;
+    const fieldValue = (() => {
+        // If the field is part of an array the value is passed in as an object with the field name as key
+        // this is imposed by react-hook-form. We try to detect this and extract the actual value
+        if (_.isObject(value) && _.has(value, fieldName)) {
+            return _.get(value, fieldName);
+        } else if (value === null) {
+            // When the value is set to null (-eg the field isNullable and the value is removed)
+            // we display '' to avoid letting the field become 'uncontrolled' (eg. getting the console error ".. a component is
+            // going from controlled to uncontrolled ...").
+            return '';
+        }
+        return value;
+    })();
 
     return (
         <input
             data-testid={pydanticFormField.id}
             onBlur={onBlur}
             onChange={(t) => {
-                const value = parseInt(t.currentTarget.value);
+                const value = t.currentTarget.value
+                    ? parseInt(t.currentTarget.value)
+                    : '';
+
                 onChange(value);
             }}
             disabled={disabled}
