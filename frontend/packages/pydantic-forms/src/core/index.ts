2fad3f244663329be7668dd4ed2041efb61fb3ea
Ruben van Leeuwen <ruben@sinin.nl>
2026-04-07 17:12:28 +0200

    Restores exports

--- frontend/packages/pydantic-forms/src/core/index.ts
+++ frontend/packages/pydantic-forms/src/core/index.ts
@@ -1,3 +1,7 @@
 export * from './PydanticFormHandler';
 export * from './hooks';
 export * from './helper';
+export * from './getMatcher';
+export * from './getPydanticFormComponents';
+export * from './getClientSideValidationRule';
+export * from './fieldToComponentMatcher';
