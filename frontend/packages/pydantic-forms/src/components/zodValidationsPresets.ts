8aac2d1d26595746d967f812785e7e53028bd7e2
Ruben van Leeuwen <ruben@sinin.nl>
2026-04-30 17:56:35 +0200

    131: Hardcode error messages in zod rules to make sure they always show up in consuming applications

--- frontend/packages/pydantic-forms/src/components/zodValidationsPresets.ts
+++ frontend/packages/pydantic-forms/src/components/zodValidationsPresets.ts
@@ -18,16 +18,23 @@ export const zodValidationPresets: PydanticFormZodValidationPresets = {
 
         let validationRule = z.string().trim();
         if (minLength) {
-            validationRule = validationRule?.min(minLength);
+            validationRule = validationRule?.min(minLength, {
+                error: `Too small: expected string to have >= ${minLength} characters`,
+            });
         }
 
         if (maxLength) {
-            validationRule = validationRule?.max(maxLength);
+            validationRule = validationRule?.max(maxLength, {
+                error: `Too big: expected string to have <= ${maxLength} characters`,
+            });
         }
 
         if (pattern) {
             try {
-                validationRule = validationRule?.regex(new RegExp(pattern));
+                validationRule = validationRule?.regex(
+                    new RegExp(pattern),
+                    `Invalid pattern: ${pattern}`,
+                );
             } catch (error) {
                 console.error(
                     'Could not parse validation rule regex',
@@ -58,23 +65,33 @@ export const zodValidationPresets: PydanticFormZodValidationPresets = {
         let validationRule = z.number().int();
 
         if (minimum) {
-            validationRule = validationRule.gte(minimum);
+            validationRule = validationRule.gte(minimum, {
+                error: `Too small: expected number to be >= ${minimum}`,
+            });
         }
 
         if (exclusiveMinimum) {
-            validationRule = validationRule.gt(exclusiveMinimum);
+            validationRule = validationRule.gt(exclusiveMinimum, {
+                error: `Too small: expected number to be > ${exclusiveMinimum}`,
+            });
         }
 
         if (maximum) {
-            validationRule = validationRule.lte(maximum);
+            validationRule = validationRule.lte(maximum, {
+                error: `Too big: expected number to be <= ${maximum}`,
+            });
         }
 
         if (exclusiveMaximum) {
-            validationRule = validationRule.lt(exclusiveMaximum);
+            validationRule = validationRule.lt(exclusiveMaximum, {
+                error: `Too big: expected number to be < ${exclusiveMaximum}`,
+            });
         }
 
         if (multipleOf) {
-            validationRule = validationRule.multipleOf(multipleOf);
+            validationRule = validationRule.multipleOf(multipleOf, {
+                error: `Invalid number: must be a multiple of ${multipleOf}`,
+            });
         }
 
         return validationRule;
